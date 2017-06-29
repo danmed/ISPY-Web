d100681bc76e30305a7f53b55ce0fc4b2d96163b
danmed <danmed@gmail.com>
2017-06-29 16:16:43 +0100

    Add files via upload

---
+++ cameras.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Cameras</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/thumbnail-gallery.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+
+<body>
+<?php
+$date7 = new DateTime('7 days ago');
+$date6 = new DateTime('6 days ago');
+$date5 = new DateTime('5 days ago');
+$date4 = new DateTime('4 days ago');
+$date3 = new DateTime('3 days ago');
+$date2 = new DateTime('2 days ago');
+$date1 = new DateTime('1 days ago');
+$date = new DateTime('Today');
+$camera = $_GET['cam'];
+?>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                 <a class="navbar-brand" href="index.php">Cameras</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+
+				<li><a href="#">Dates</a></li>
+						<li><a href="<?PHP echo $_SERVER['PHP_SELF'];?>?date=<?php echo $date->format('Y-m-d')?>&cam=<?PHP echo $camera;?>"><?php echo $date->format('d-m-Y')?></a></li>
+						<li><a href="<?PHP echo $_SERVER['PHP_SELF'];?>?date=<?php echo $date1->format('Y-m-d')?>&cam=<?PHP echo $camera;?>""><?php echo $date1->format('d-m-Y')?></a></li>
+						<li><a href="<?PHP echo $_SERVER['PHP_SELF'];?>?date=<?php echo $date2->format('Y-m-d')?>&cam=<?PHP echo $camera;?>""><?php echo $date2->format('d-m-Y')?></a></li>
+						<li><a href="<?PHP echo $_SERVER['PHP_SELF'];?>?date=<?php echo $date3->format('Y-m-d')?>&cam=<?PHP echo $camera;?>""><?php echo $date3->format('d-m-Y')?></a></li>
+						<li><a href="<?PHP echo $_SERVER['PHP_SELF'];?>?date=<?php echo $date4->format('Y-m-d')?>&cam=<?PHP echo $camera;?>""><?php echo $date4->format('d-m-Y')?></a></li>
+						<li><a href="<?PHP echo $_SERVER['PHP_SELF'];?>?date=<?php echo $date5->format('Y-m-d')?>&cam=<?PHP echo $camera;?>""><?php echo $date5->format('d-m-Y')?></a></li>
+						<li><a href="<?PHP echo $_SERVER['PHP_SELF'];?>?date=<?php echo $date6->format('Y-m-d')?>&cam=<?PHP echo $camera;?>""><?php echo $date6->format('d-m-Y')?></a></li>
+                  </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+<?php
+ 
+$date = $_GET['date'];
+$camera = $_GET['cam'];
+
+if($camera == "house"){
+	$dir='files/video/HOUSECAM';
+	$title='House';
+						}
+						else
+						{
+	$dir='files/video/GARAGECAM';
+	$title='Garage';
+	}
+$files = array_slice(scandir($dir,1), 2);
+$files =  preg_grep('/^([^.])/', $files);
+?>
+
+            <div class="col-lg-12">
+                <h1 class="page-header"><?PHP echo $title;?> Camera - <?PHP echo $date;?></h1>
+            </div>
+
+
+<?PHP
+foreach($files as $ind_file){
+if($ind_file != 'data.xml'){
+if (strpos($ind_file, $date) !== false) {
+$thumb = preg_replace('/\\.[^.\\s]{3,4}$/', '', $ind_file);
+$thumbpath = $dir . '/thumbs/' . $thumb . '.jpg';
+?>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                    <a class="thumbnail" href="display.php?file=<?php echo $dir."/".$ind_file;?>"><img class="img-responsive" src="<?php echo $thumbpath?>" width="400" height="300" alt=""></a><center><?PHP echo $thumb;?>        
+            </div>
+<?php
+}}}
+?>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Cameras</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/thumbnail-gallery.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<?PHP 
+$date = new DateTime('Today');
+?>
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                 <a class="navbar-brand" href="index.php">Cameras</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+
+
+                  </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h1 class="page-header">Cameras</h1>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                    <a class="thumbnail" href="cameras.php?cam=house&date=<?PHP echo $date->format('Y-m-d'); ?>"><img class="img-responsive" src="http://danmed.duckdns.org:88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=danmed&pwd=dm312217" width="400" height="300" alt=""></a>
+                
+            </div>
+			<div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                    <a class="thumbnail" href="cameras.php?cam=garage&date=<?PHP echo $date->format('Y-m-d'); ?>"><img class="img-responsive" src="http://danmed.duckdns.org:81/snapshot.cgi?user=danmed&pwd=dm312217" width="400" height="300" alt=""></a>
+                </a>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
